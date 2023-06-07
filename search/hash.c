dab22cb8297e6dc8e5f8288673d2da7f9b6c410d
aapdo <jade04342@gmail.com>
2023-06-07 21:38:31 +0900

    correct

--- search/hash.c
+++ search/hash.c
@@ -122,6 +122,7 @@ void search(){
             return;
         }
     }
+
     printf("\n------------ Result ------------\n");
     printf("Keyword: %s\n", oneWord);
     printf("Total number of documents: %d\n", hashTable[index][0]->cnt);
